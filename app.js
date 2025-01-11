96402b31f41994efd3c428a42b1f6958c64447da
nguyenthanhtrung <tn647923@gmail.com>
2025-01-12 00:12:46 +0700

    update

--- app.js
+++ app.js
@@ -5,20 +5,16 @@ const productsRouter = require('./routes/products');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/products', productsRouter);
 
-// Basic error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Đã xảy ra lỗi!' });
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
 });
